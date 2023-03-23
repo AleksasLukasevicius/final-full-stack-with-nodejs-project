0bd88434d4ec5bbf131fc614a9d7e07dd1b112da
AleksasLukasevicius <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2023-03-23 17:11:15 +0200

    events context

--- src/components/EventsContext/EventsContext.tsx
+++ src/components/EventsContext/EventsContext.tsx
@@ -4,6 +4,6 @@ import { TEventsContext } from "./types";
 const INITIAL_VALUE = {
   events: [],
   dispatch: () => {},
-} as TEventsContext;
+} as const;
 
 export const EventsContext = createContext<TEventsContext>(INITIAL_VALUE);
