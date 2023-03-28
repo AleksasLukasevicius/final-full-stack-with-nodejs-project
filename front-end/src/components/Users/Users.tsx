c00d43aa63ed93843bb351fc5fde6a7e7b2d3b6f
ALEKSAS LUKAŠEVIČIUS <aleksas.lukasevicius@codeacademylt.onmicrosoft.com>
2023-03-28 22:59:26 +0300

    edit user

--- front-end/src/components/Users/Users.tsx
+++ front-end/src/components/Users/Users.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import { useState } from "react";
 import { UsersTable } from "./UsersTable";
 
